fe487c7f8b2edaa1d6185a397a5afd4a7428eb1e
MostlySimple <mostlysimple@gmail.com>
2026-03-15 17:43:55 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Johnny Hubbard on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballTests/FootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by Johnny Hubbard on 3/15/26.
+//
+
+import Testing
+@testable import Football
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
